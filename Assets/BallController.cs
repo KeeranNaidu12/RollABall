fb43f82311c92bed8c6d1f708cc5f44ac96c0468
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-01-20 12:45:28 -0800

    Added walls, removed InputManager Script and applied textures
    Walls have been added so the ball can't fall off the plane.I added textures to the walls and the ball. I have removed the InputManager script.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,16 +4,15 @@ using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BallController : MonoBehaviour
 {
     public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public UnityEvent<UnityEngine.Vector2> onMove = new UnityEvent<UnityEngine.Vector2>();
+    void Start(){
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    }
+
+    void Update(){
         UnityEngine.Vector2 inputVector = UnityEngine.Vector2.zero;
         if(Input.GetKey(KeyCode.W)){
             inputVector += UnityEngine.Vector2.up;
@@ -21,16 +20,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if(Input.GetKey(KeyCode.S)){
             inputVector += UnityEngine.Vector2.down;
         }
-        if(Input.GetKey(KeyCode.D)){
-            inputVector += UnityEngine.Vector2.right;
-        }
         if(Input.GetKey(KeyCode.A)){
             inputVector += UnityEngine.Vector2.left;
         }
-        UnityEngine.Vector3 inputXYZPlane = new UnityEngine.Vector3(inputVector.x,0,inputVector.y);
-        sphereRigidbody.AddForce(inputXYZPlane*ballSpeed);
+        if(Input.GetKey(KeyCode.D)){
+            inputVector += UnityEngine.Vector2.right;
+        }
 
-        onMove?.Invoke(inputVector);
-        
+        UnityEngine.Vector3 inputXYZPlane = new UnityEngine.Vector3(inputVector.x,0,inputVector.y);
+        sphereRigidbody.AddForce(inputXYZPlane);
+
     }
 }
