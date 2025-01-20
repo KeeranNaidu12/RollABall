f6fbdba8c627e4d23e32fa6106f20fd37f260ffc
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-01-19 23:04:57 -0800

    Added movement
    I added movement to my ball

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,16 +1,35 @@
+using System.Numerics;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
+    private UnityEngine.Vector2 inputVector;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the start method");
+        inputVector = new UnityEngine.Vector2(0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the Update method");
+        if(Input.GetKey(KeyCode.W)){
+            inputVector += UnityEngine.Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            inputVector += UnityEngine.Vector2.down;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputVector += UnityEngine.Vector2.right;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            inputVector += UnityEngine.Vector2.left;
+        }
+        UnityEngine.Vector3 inputXYZPlane = new UnityEngine.Vector3(inputVector.x,0,inputVector.y);
+        sphereRigidbody.AddForce(inputXYZPlane*ballSpeed);
+        
     }
 }
