717b81d53df64515f3c50d85cf71493f518f95de
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-01-19 23:37:57 -0800

    I made some more changes

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -4,7 +4,7 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] float ballSpeed;
-    public void MoveBall(Vector2 input){
+    public void MoveBall(UnityEngine.Vector2 input){
         Vector3 inputXYZPlane = new(input.x,0,input.y);
         sphereRigidbody.AddForce(inputXYZPlane*ballSpeed);
     }
